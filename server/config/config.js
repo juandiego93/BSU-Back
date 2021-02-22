67b2997cbb240e614c96921d94e5ac7dcf2d469e
juanCoxti <juan.osorio@coxti.net>
2021-02-22 12:24:01 -0500

    first commit

---
+++ server/config/config.js
@@ -0,0 +1,24 @@
+//PORT
+process.env.PORT = process.env.PORT || 3000
+
+//ENVIROMENT
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+
+//DB
+let urlDB
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost/test'
+} else {
+    urlDB = process.env.MONGO_URI
+}
+process.env.URLDB = urlDB;
+
+//SEDD de Autenticacion
+
+process.env.SEED = process.env.SEED || 'secret-de-desarrollo'
+
+// Vencimiento Token
+// 60 seg * 60 min * 24h * 30d
+// process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30
+process.env.CADUCIDAD_TOKEN = '48h'
---
+++ server/server.js
@@ -0,0 +1,31 @@
+require('./config/config')
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const app = express()
+
+app.use(cors())
+app.use(require('./routes/index'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+app.listen((process.env.PORT), function () {
+    console.log('Server running:', process.env.PORT);
+})
+
+mongoose.set('useCreateIndex', true)
+mongoose.connect(
+    process.env.URLDB,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    },
+    err => {
+        if (!err) {
+            console.log('DB conneccted', process.env.URLDB);
+        } else {
+            console.log('Error connecting', err);
+        }
+    })
